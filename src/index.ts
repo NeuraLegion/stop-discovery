3c5e846a2d9bf237d1527104a3dce240d8191401
Bram Kortooms <bramkortooms@gmail.com>
2024-12-16 09:30:41 +0200

    feat: create workflow to run Bright discovery

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import * as core from '@actions/core';
+import * as rm from 'typed-rest-client/RestClient';
+
+const apiToken = core.getInput('api_token');
+const discoveryId = core.getInput('discovery');
+const projectId = core.getInput('project');
+const hostname = core.getInput('hostname');
+
+const baseUrl = hostname ? `https://${hostname}` : 'https://app.brightsec.com';
+const restc = new rm.RestClient('GitHub Actions', baseUrl);
+
+async function stopDiscovery(uuid: string) {
+  try {
+    const options = {
+      additionalHeaders: { Authorization: `Api-Key ${apiToken}` }
+    };
+    const payload = {
+      action: 'stop'
+    };
+    const restRes = await restc.update(
+      `api/v2/projecs/${projectId}/discoveries/${uuid}`,
+      payload,
+      options
+    );
+    core.info(`Was succesfully stopped. Code ${restRes.statusCode}.`);
+  } catch (err: any) {
+    core.setFailed(`Failed (${err.statusCode}) ${err.message}`);
+  }
+}
+
+stopDiscovery(discoveryId);
